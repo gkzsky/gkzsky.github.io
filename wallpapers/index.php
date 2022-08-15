37c8923174784b9d5c906210f2123cf56e0d120a
Jul <50765908+gkzsky@users.noreply.github.com>
2022-08-15 23:36:10 +0800

    Update index.php

--- wallpapers/index.php
+++ wallpapers/index.php
@@ -3,22 +3,22 @@
 <body>
 
 <?php 
-$myLinks = array("https://takagen99.github.io/wallpapers/01.png", 
-    "https://takagen99.github.io/wallpapers/02.png",
-    "https://takagen99.github.io/wallpapers/03.jpg",
-    "https://takagen99.github.io/wallpapers/04.jpg",
-    "https://takagen99.github.io/wallpapers/05.jpg",
-    "https://takagen99.github.io/wallpapers/06.jpg",
-    "https://takagen99.github.io/wallpapers/07.png",
-    "https://takagen99.github.io/wallpapers/08.png",
-    "https://takagen99.github.io/wallpapers/09.png",
-    "https://takagen99.github.io/wallpapers/10.png",
-    "https://takagen99.github.io/wallpapers/11.jpg",
-    "https://takagen99.github.io/wallpapers/12.png",
-    "https://takagen99.github.io/wallpapers/13.jpg",
-    "https://takagen99.github.io/wallpapers/14.jpg",
-    "https://takagen99.github.io/wallpapers/15.jpg",
-	"https://takagen99.github.io/wallpapers/16.jpg");
+$myLinks = array("https://gkzsky.github.io/wallpapers/01.png", 
+    "https://gkzsky.github.io/wallpapers/02.png",
+    "https://gkzsky.github.io/wallpapers/03.jpg",
+    "https://gkzsky.github.io/wallpapers/04.jpg",
+    "https://gkzsky.github.io/wallpapers/05.jpg",
+    "https://gkzsky.github.io/wallpapers/06.jpg",
+    "https://gkzsky.github.io/wallpapers/07.png",
+    "https://gkzsky.github.io/wallpapers/08.png",
+    "https://gkzsky.github.io/wallpapers/09.png",
+    "https://gkzsky.github.io/wallpapers/10.png",
+    "https://gkzsky.github.io/wallpapers/11.jpg",
+    "https://gkzsky.github.io/wallpapers/12.png",
+    "https://gkzsky.github.io/wallpapers/13.jpg",
+    "https://gkzsky.github.io/wallpapers/14.jpg",
+    "https://gkzsky.github.io/wallpapers/15.jpg",
+	"https://gkzsky.github.io/wallpapers/16.jpg");
 
 $randomRedirection = $myLinks[array_rand($myLinks)];
 header("Location: $randomRedirection");
